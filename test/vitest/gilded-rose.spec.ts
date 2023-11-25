add9ebdaa483dcd455c621322479938b4903906f
Brando Vidal <yisusdracon@gmail.com>
2023-11-25 18:00:30 -0500

    ⚒  complete test with vitest

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -37,6 +37,7 @@ describe('Gilded Rose Should', () => {
     expect(whateverItem.quality).toEqual(0)
   })
 
+
   it('Test Aged Brie increases quality with age', () => {
     const agedBrie = new Item('Aged Brie', 5, 1)
 
@@ -44,5 +45,53 @@ describe('Gilded Rose Should', () => {
    gildedRose.updateQuality()
 
     expect(agedBrie.quality).toEqual(2)
+  })
+
+  it('Test Aged Brie increases quality past fifty', () => {
+    const agedBrie = new Item('Aged Brie', 5, 50)
+
+    const gildedRose = new GildedRose([agedBrie])
+   gildedRose.updateQuality()
+
+    expect(agedBrie.quality).toEqual(50)
+  })
+
+
+  it('Test Sulfuras never changes', () => {
+    const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 25)
+
+    const gildedRose = new GildedRose([sulfuras])
+   gildedRose.updateQuality()
+
+    expect(sulfuras.quality).toEqual(25)
+    expect(sulfuras.sellIn).toEqual(0)
+  })
+
+
+  it('Test Backstage pass increases quality by one if sell by greater than 10', () => {
+    const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20)
+
+    const gildedRose = new GildedRose([backstagePasses])
+   gildedRose.updateQuality()
+
+    expect(backstagePasses.quality).toEqual(21)
+  })
+
+  it('Test Backstage pass increases quality by two if sell by smaller than 5', () => {
+    const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)
+
+    const gildedRose = new GildedRose([backstagePasses])
+   gildedRose.updateQuality()
+
+    expect(backstagePasses.quality).toEqual(23)
+  })
+
+  it('Test Backstage pass loses value after sell by passes', () => {
+    const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)
+
+    const gildedRose = new GildedRose([backstagePasses])
+   gildedRose.updateQuality()
+
+    expect(backstagePasses.quality).toEqual(0)
   })
 })
