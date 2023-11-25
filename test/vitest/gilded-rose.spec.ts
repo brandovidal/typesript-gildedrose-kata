e3053d00155392d649c4dcc69753cbcf7c14edf2
Brando Vidal <yisusdracon@gmail.com>
2023-11-25 17:50:04 -0500

    ⚒  upgrade tests

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -1,9 +1,48 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from '@/gilded-rose'
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
-});
+describe('Gilded Rose Should', () => {
+  it('Test that sellIn value is decreased', () => {
+    const whateverItem = new Item('whatever', 10, 0)
+
+    const gildedRose = new GildedRose([whateverItem])
+   gildedRose.updateQuality()
+
+    expect(whateverItem.sellIn).toEqual(9)
+  })
+
+  it('Test that quality value is decreased', () => {
+    const whateverItem = new Item('whatever', 1, 10)
+
+    const gildedRose = new GildedRose([whateverItem])
+   gildedRose.updateQuality()
+
+    expect(whateverItem.quality).toEqual(9)
+  })
+
+  it('Test that quality decreases twice as much when sellIn by is passed', () => {
+    const whateverItem = new Item('whatever', 0, 10)
+
+    const gildedRose = new GildedRose([whateverItem])
+   gildedRose.updateQuality()
+
+    expect(whateverItem.quality).toEqual(8)
+  })
+
+  it('Test that quality is never negative', () => {
+    const whateverItem = new Item('whatever', 0, 0)
+
+    const gildedRose = new GildedRose([whateverItem])
+   gildedRose.updateQuality()
+
+    expect(whateverItem.quality).toEqual(0)
+  })
+
+  it('Test Aged Brie increases quality with age', () => {
+    const agedBrie = new Item('Aged Brie', 5, 1)
+
+    const gildedRose = new GildedRose([agedBrie])
+   gildedRose.updateQuality()
+
+    expect(agedBrie.quality).toEqual(2)
+  })
+})
